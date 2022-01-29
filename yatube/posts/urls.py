da579db42b192f359a9163cebc78a192acbd5fb0
Viktor Ermakov <ermakov.viktor.v@gmail.com>
2022-01-29 20:44:03 +0300

    на верном пути, наверно!

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,8 +1,10 @@
 # posts/urls.py
 from django.urls import path
 from . import views
+app_name = 'posts'
 
 urlpatterns = [
-    path('', views.index),
-    path('group/<slug:slug>/', views.group_posts)
+    path('', views.index, name='index'),
+    path('group_list.html', views.group_list, name='group_list'),
+    path('group/<slug:slug>/', views.group_posts, name='group'),
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,7 +4,24 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Главная страница Виктора')
+    template = 'posts/index.html'
+    title = 'Это главная страница проекта Yatube'
+    context = {
+        'title': title
+    }
+    return render(request, template, context)
+
+def group_list(request):
+    template = 'posts/group_list.html'
+    return render(request, template)
 
 def group_posts(reguest, slug):
-    return HttpResponse(f'Это группа {slug}')
+    template = 'posts/group_posts.html'
+    title = 'Здесь будет информация о группах проекта Yatube'
+    text = HttpResponse(f'Это группа {slug}')
+    context = {
+        'title': title,
+        'text': text,
+    }
+    return render(reguest, template, context)
+
